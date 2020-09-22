06307bb42277f65c948dae1b2bcc6a395a3ce38e
Eytan Canzini <ethan.canzini@gmail.com>
2020-09-22 16:49:52 +0100

    Ended regression cycle. Not worth it

--- regression/regression_test_v2.py
+++ regression/regression_test_v2.py
@@ -10,37 +10,42 @@ sns.set()
 time_series = 182
 
 def drop_columns(hist):
-    hist = hist.drop(columns=['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'])
-    return hist
+    return hist.drop(columns=['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'])
 
 def train_size(num, df):
-    length = df.shape[0] - num
-    return length
+    return df.shape[0] - num
 
 def create_sets(df):
-    df_train = df.index.to_numpy().reshape(-1, 1)
-    for i in df_train:
-        print(i)
-    df_valid = df['Close'].to_numpy().reshape(-1, 1)
-    return df_train, df_valid
+    df_x = df['Date'].to_numpy().reshape(-1, 1)
+    df_y = df['Close'].to_numpy().reshape(-1, 1)
+    return df_x, df_y
 
 
 data = yf.Ticker('MSFT')
 
 data_hist = data.history(period="max")
 df = drop_columns(data_hist)
-train = df[:train_size(time_series, df)]
-valid = df[train_size(time_series, df):]
+
+new_data = pd.DataFrame(index=range(0, len(df)), columns=['Date', 'Close'])
+for i in range(0, len(df)):
+    new_data['Date'][i] = str(df.index[i]).split(' ')[0]
+    new_data['Close'][i] = df['Close'][i]
+
+# print(new_data)
+
+train = new_data[:train_size(time_series, new_data)]
+valid = new_data[train_size(time_series, new_data):]
 
 x_train, y_train = create_sets(train)
 x_valid, y_valid = create_sets(valid)
-print(x_train, y_train)
+
+y_train = train.index
+
+
+# print(x_train, y_train)
 
 model = LinearRegression().fit(x_train, y_train)
 
 preds = model.predict(x_valid)
 rms = np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
 print(rms)
-
-
-
